0055e2aded1f990d0ccd8bfcc014426c527a4775
Dmitrijs Vasilevskis <dmitrijs.vasilevskis@scandiweb.com>
2026-05-25 22:52:39 +0300

    remove redundand cli command

--- apps/backend/docker-entrypoint.sh
+++ apps/backend/docker-entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 echo "Building @uno/shared..."
 npm run build --workspace=@uno/shared
 
-echo "Applying database schema..."
-npx prisma db push --accept-data-loss
-
 cd /app
 echo "Starting backend dev server..."
 exec npm run dev --workspace=@uno/backend
