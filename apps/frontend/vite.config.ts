fb071e908177a9586ac546eb72cea68494b5b7ab
Dmitrijs Vasilevskis <dmitrijs.vasilevskis@scandiweb.com>
2026-05-26 12:03:32 +0300

    runtime fix

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: Number(process.env.PORT) || 5173,
-    watch: {
-      usePolling: process.env.CHOKIDAR_USEPOLLING === 'true',
-    },
-  },
-  optimizeDeps: {
-    exclude: ["@uno/shared"],
-  }
 })
