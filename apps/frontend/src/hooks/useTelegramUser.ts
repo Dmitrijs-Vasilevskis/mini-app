88d09c3b15baa5d0ad0d6d1e2c90a75fca1fead6
Dmitrijs Vasilevskis <dmitrijs.vasilevskis@scandiweb.com>
2026-06-08 21:45:49 +0300

    uncomment tg.requestFullscreen

--- apps/frontend/src/hooks/useTelegramUser.ts
+++ apps/frontend/src/hooks/useTelegramUser.ts
@@ -32,9 +32,9 @@ export function useTelegramUser() {
       tg.ready();
       tg.expand();
 
-      // if (tg.requestFullscreen) {
-      //   tg.requestFullscreen();
-      // }
+      if (tg.requestFullscreen) {
+        tg.requestFullscreen();
+      }
 
       const realUser = tg.initDataUnsafe?.user;
       if (realUser) {
