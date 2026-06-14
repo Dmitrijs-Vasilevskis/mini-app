7e6788f1f9fac3884485878183ef97ada69692e9
Dmitrijs Vasilevskis <dmitrijs.vasilevskis@scandiweb.com>
2026-06-14 16:49:56 +0300

    FE - updates in a lobby UI, add leave button and change the position of actions

--- apps/frontend/src/components/lobby/LobbyActions.tsx
+++ apps/frontend/src/components/lobby/LobbyActions.tsx
@@ -4,26 +4,40 @@ interface Props {
   isReady: boolean;
   isHost: boolean;
   canStart: boolean;
+  onLeave: () => void;
 }
-export function LobbyActions({ isReady, isHost, canStart }: Props) {
+export function LobbyActions({ isReady, isHost, canStart, onLeave }: Props) {
   return (
-    <div className="w-full flex gap-12 pt-4">
+    <div className="w-full flex flex-col gap-6 pt-4">
       {isHost && (
-        <button
-          onClick={() => colyseusService.startGame()}
-          disabled={!canStart}
-          className={`bg-yellow-500 w-full text-black px-5 py-3 rounded-lg font-bold ${!canStart && "grayscale"}`}
-        >
-          Start Game
-        </button>
+        <div className="flex w-full">
+          <button
+            onClick={() => colyseusService.startGame()}
+            disabled={!canStart}
+            className={`bg-yellow-500 transition active:scale-95 w-full text-black px-5 py-3 rounded-lg font-bold ${!canStart && "grayscale"}`}
+          >
+            Start Game
+          </button>
+        </div>
       )}
 
-      <button
-        onClick={() => colyseusService.toggleReady()}
-        className="bg-green-600 px-5 py-3 rounded-lg font-semibold w-full"
-      >
-        {isReady ? "Not Ready" : "Ready"}
-      </button>
+      <div className="flex w-full">
+        <button
+          onClick={() => colyseusService.toggleReady()}
+          className="bg-green-600 transition active:scale-95 px-5 py-3 rounded-lg font-semibold w-full"
+        >
+          {isReady ? "Not Ready" : "Ready"}
+        </button>
+      </div>
+
+      <div className="flex w-full">
+        <button
+          onClick={onLeave}
+          className="w-full px-5 py-3 rounded-lg bg-red-500/15 border-red-500/30 text-red-300 font-semebold transition hover:bg-red-500/25 active:scale-95"
+        >
+          Leave Room
+        </button>
+      </div>
     </div>
   );
 }
--- apps/frontend/src/components/lobby/LobbyHeader.tsx
+++ apps/frontend/src/components/lobby/LobbyHeader.tsx
@@ -1,22 +1,27 @@
 interface Props {
-    roomCode: string | null;
-    onClick: () => void;
+  roomCode: string | null;
+  onClick: () => void;
+  onLeave: () => void;
 }
 
 export function LobbyHeader({ roomCode, onClick }: Props) {
   return (
-    <div className=" w-full max-w-md flex justify-around mb-4">
+    <div className="w-full max-w-md flex flex-row mb-4 justify-center gap-4">
       <div className="text-center">
         <div className="text-sm opacity-70">Room Code</div>
-        <div className=" text-4xl font-black tracking-widest">{roomCode ?? ""}</div>
+        <div className=" text-4xl font-black tracking-widest">
+          {roomCode ?? ""}
+        </div>
       </div>
 
-      <button
-        onClick={onClick}
-        className=" bg-blue-600 px-5 py-3 rounded-lg font-semibold"
-      >
-        Copy Room Code
-      </button>
+      <div className="flex justify-center">
+        <button
+          onClick={onClick}
+          className="transition active:scale-95 bg-blue-600 px-5 py-3 rounded-lg font-semibold"
+        >
+          Copy Room Code
+        </button>
+      </div>
     </div>
   );
 }
