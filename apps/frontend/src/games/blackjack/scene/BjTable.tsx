cc4011e9cb34c9d4e02df24d9f4b46732f580c9f
Dmitrijs Vasilevskis <dmitrijs.vasilevskis@scandiweb.com>
2026-08-24 14:00:57 +0300

    FE - BJ 3D scene with an oval table

---
+++ apps/frontend/src/games/blackjack/scene/BjTable.tsx
@@ -0,0 +1,47 @@
+import { Canvas } from "@react-three/fiber";
+import { useBjLocalPlayer, useBjPlayers } from "../hooks";
+import { OrbitControls } from "@react-three/drei";
+import { OvalTable } from "../../../components/games/bj/table/OvalTable";
+
+export function BjTable() {
+  const localPlayer = useBjLocalPlayer();
+  const players = useBjPlayers();
+
+  if (!localPlayer) {
+    return null;
+  }
+
+  const otherPlayers = players.filter((p) => p.id !== localPlayer.id);
+
+  return (
+    <Canvas
+      camera={{
+        position: [0, 8, 8],
+        fov: 40,
+      }}
+    >
+      <ambientLight intensity={0.4} />
+
+      <hemisphereLight intensity={0.8} color="#bcd6ff" groundColor="#2b2b3a" />
+
+      <directionalLight position={[5, 10, 5]} intensity={1.2} />
+      <fog attach="fog" args={["#1e2633", 8, 25]} />
+
+      {/* Floor */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.2, 0]}>
+        <planeGeometry args={[30, 30]} />
+        <meshStandardMaterial color="#283042" />
+      </mesh>
+
+      {/* Table */}
+      <OvalTable />
+
+      <OrbitControls
+        enablePan={false}
+        minDistance={6}
+        maxDistance={12}
+        maxPolarAngle={Math.PI / 2.2}
+      />
+    </Canvas>
+  );
+}
